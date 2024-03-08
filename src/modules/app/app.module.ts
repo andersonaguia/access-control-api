76d0f483e25544aac63834b7da79b87f6c68f62b
andersonaguia <andersonlaguiar@gmail.com>
2024-03-08 15:56:10 -0300

    typeorm config

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'src/common/env';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { dataSourceOptions } from 'src/core/database/data-source';
 @Module({
-  imports: [
-    ConfigModule.forRoot({
+  imports: [  
+  ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
     }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', '..', 'public'),
+    }),
+    TypeOrmModule.forRoot({ autoLoadEntities: true, ...dataSourceOptions }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

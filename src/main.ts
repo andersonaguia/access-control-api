a4168fcba056bef68d47ec8d4e8c0cbc95d7ec8c
andersonaguia <andersonlaguiar@gmail.com>
2024-03-08 14:11:30 -0300

    environment variables config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SocketIoAdapter } from './core/adapters/socketIo/socket.adapter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('port') || 3000;
+  const SOCKET_PORT = configService.get<number>('socketPort') || 8001;
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true
+    }),
+  );
+  
+  app.enableCors();
+
+  app.useWebSocketAdapter(new SocketIoAdapter(app, configService));
+
+  await app.listen(PORT).then(() => {
+    Logger.log(`Server is running on port ${PORT}`);
+    Logger.log(`WebSocket is running on port ${SOCKET_PORT}`);
+  });
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ConfigModule } from '@nestjs/config';
+import configuration from 'src/common/env';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

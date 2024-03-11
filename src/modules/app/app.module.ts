ee17f16b5939e4fcd717e90a2786ea7daebd5504
andersonaguia <andersonlaguiar@gmail.com>
2024-03-11 15:08:52 -0300

    use doors module in app

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -6,9 +6,10 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { dataSourceOptions } from 'src/core/database/data-source';
 import { AuthModule } from 'src/core/auth/auth.module';
+import { DoorsModule } from '../doors/doors.module';
 @Module({
-  imports: [  
-  ConfigModule.forRoot({
+  imports: [
+    ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
     }),
@@ -17,6 +18,7 @@ import { AuthModule } from 'src/core/auth/auth.module';
     }),
     TypeOrmModule.forRoot({ autoLoadEntities: true, ...dataSourceOptions }),
     AuthModule,
+    DoorsModule,
   ],
   controllers: [],
   providers: [],
